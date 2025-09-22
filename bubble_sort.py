51fd666d4cef7d1ce5acebad1f417051db28874e
Tenzin <124920785+Tenzinyo@users.noreply.github.com>
2025-09-22 15:40:40 -0700

    programming dsa concepts

---
+++ bubble_sort.py
@@ -0,0 +1,9 @@
+def bubblesort():
+    array = [2,3,6,4,1]
+    n = len(array)
+    for i in range(n-1):
+        for j in range(n-i-1):
+            if array[j]>array[j+1]:
+                array[j],array[j+1] = array[j+1],array[j]
+            
+            
