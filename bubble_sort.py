b2e5a9924107e1181f57cad214c1294af1ae9380
Tenzin <124920785+Tenzinyo@users.noreply.github.com>
2025-09-22 15:41:35 -0700

    following lint standards

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,9 +1,7 @@
 def bubblesort():
-    array = [2,3,6,4,1]
+    array = [2, 3, 6, 4, 1]
     n = len(array)
-    for i in range(n-1):
-        for j in range(n-i-1):
-            if array[j]>array[j+1]:
-                array[j],array[j+1] = array[j+1],array[j]
-            
-            
+    for i in range(n - 1):
+        for j in range(n - i - 1):
+            if array[j] > array[j + 1]:
+                array[j], array[j + 1] = array[j + 1], array[j]
