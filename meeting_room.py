466898a7b18063ed37a1bb280514e523554cdbcf
Tenzin <124920785+Tenzinyo@users.noreply.github.com>
2025-10-11 02:10:48 -0400

    medium

---
+++ meeting_room.py
@@ -0,0 +1,12 @@
+def meeting(self,intervals):
+    self.start = start
+    self.end  =end
+    intervals.sort(key = lambda i:i.start)
+    for i in range(1,len(intervals)):
+        i1 = intervals[i-1]
+        i2 = intervals[i]
+        if i1.end> i2.start:
+            return False
+    return True
+        
+    
