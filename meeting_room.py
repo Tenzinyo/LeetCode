8ca150d618784ffa455aede438fa37fe8d79ae86
Tenzin <124920785+Tenzinyo@users.noreply.github.com>
2025-10-18 14:57:55 -0700

    easy

--- meeting_room.py
+++ meeting_room.py
@@ -1,12 +1,15 @@
+class MR(object):
+    def __init__(self, start,end):
+        self.start = start
+        self.end= end
 def meeting(self,intervals):
-    self.start = start
-    self.end  =end
+    n = len(intervals)
     intervals.sort(key = lambda i:i.start)
-    for i in range(1,len(intervals)):
+    for i in range(1,n):
         i1 = intervals[i-1]
         i2 = intervals[i]
         if i1.end> i2.start:
             return False
     return True
         
-    
+        
