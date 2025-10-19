2c295303112effe3ba85674bc4dc234ca3fb6f55
Tenzin <124920785+Tenzinyo@users.noreply.github.com>
2025-10-19 10:44:18 -0700

    quick sort algth

--- kthlargest.py
+++ kthlargest.py
@@ -1,17 +1,20 @@
 def kthlargest(nums,k):
-    
-    k = len(nums)-k
+    n = len(nums)
+    # [3,2,1 || 5,6,4] -> nlog n 
+    # 
+    #          p
+    # |3|2|1|| 
+    # if 
     def quick(l,r):
-        p, pivot = l,nums[r]
+        p, pivot = l, nums[r]
         for i in range(l,r):
-            if nums[i]<=pivot:
+            if nums[p]<=pivot:
                 nums[i],nums[p] = nums[p],nums[i]
                 p+=1
-        
-        nums[p],nums[r] = nums[r], nums[p]
+        nums[p],nums[r] = nums[r],nums[p]
+        # to compare our p to k
         if p>k: return quick(l,p-1)
         elif p<k: return quick(p+1,r)
-        else : return nums[p]
-    return quick(0,len(nums)-1)
-               
-    
+        else: return nums[p]
+    return quick(0,n-1)
+        
